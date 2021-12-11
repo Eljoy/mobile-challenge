175ffd846990c6e1cfa3d5bda43bba9f1c9310d4
Martin <martin.wiin@gmail.com>
2021-12-11 23:38:22 +0100

    Create new project

---
+++ ExpenseFeed/ExpenseFeed/ExpenseFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseFeedApp.swift
+//  ExpenseFeed
+//
+//  Created by Martin Wiingaard on 11/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
