ae7062c86793baed5ffe0c7e62d1dfe909a027c6
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-07-09 15:19:08 +0400

    Setup base project structure

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ts', '.tsx'],
+        alias: {
+          '@design-system': ['./src/design-system'],
+          '@features': ['./src/features'],
+          '@models': ['./src/models'],
+          '@screens': ['./src/screens'],
+        },
+      },
+    ],
+  ],
+}
---
+++ app/src/features/index.ts
@@ -0,0 +1,3 @@
+import { Test } from '@models/Test'
+
+console.log('Test ', Test)
