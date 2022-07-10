81191536ce00510f7f36e1c290deb16217098110
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-07-10 18:14:05 +0400

    Setup redux-flipper

--- app/src/app/store.ts
+++ app/src/app/store.ts
@@ -1,9 +1,20 @@
+import { expenseApi } from '@api'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+  [expenseApi.reducerPath]: expenseApi.reducer,
+})
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware().concat(expenseApi.middleware)
+    if (__DEV__) {
+      const createDebugger = require('redux-flipper').default
+      middlewares.push(createDebugger())
+    }
+    return middlewares
+  },
 })
 
 export type RootState = ReturnType<typeof rootReducer>
