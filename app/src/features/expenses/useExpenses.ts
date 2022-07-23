c3eff5a96d80fb07c9aa1efe384a41b9c11dcceb
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-07-23 22:11:52 +0400

    Add useExpenses

---
+++ app/src/features/expenses/useExpenses.ts
@@ -0,0 +1,41 @@
+import { filterBy } from '@common';
+import {
+  expenseSelectors,
+  fetchExpenses,
+} from '@features/expenses/store/expensesSlice';
+import { useCallback, useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AnyAction } from 'redux';
+
+export declare namespace useExpenseList {
+  export type Props = {
+    searchKeyword?: string;
+  };
+}
+
+export const useExpenseList = ({
+  searchKeyword = '',
+}: useExpenseList.Props = {}) => {
+  const dispatch = useDispatch();
+  const expenses = useSelector(expenseSelectors.selectAll);
+
+  const triggerNext = useCallback(() => {
+    dispatch(fetchExpenses() as unknown as AnyAction);
+  }, []);
+
+  const filteredExpenses = useMemo(() => {
+    return searchKeyword
+      ? filterBy(
+          expenses,
+          ['user.first', 'user.last', 'merchant'],
+          searchKeyword
+        )
+      : expenses;
+  }, [expenses, searchKeyword]);
+
+  useEffect(() => {
+    triggerNext();
+  }, [dispatch, triggerNext]);
+
+  return { expenses: filteredExpenses, triggerNext };
+};
