b87b916005c66c61a2e09ef1b1372a8176db2f3f
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-07-23 16:27:06 +0400

    Remove index

--- app/src/models/Expense.ts
+++ app/src/models/Expense.ts
@@ -1,14 +1,13 @@
-import { Amount } from './Amount'
-import { User } from './User'
+import { Amount } from './Amount';
+import { User } from './User';
 
 export type Expense = {
-  id: string
-  amount: Amount
-  date: string
-  merchant: string
-  receipts: any[]
-  comment: string
-  category: string
-  user: User
-  index: number
-}
+  id: string;
+  amount: Amount;
+  date: string;
+  merchant: string;
+  receipts: any[];
+  comment: string;
+  category: string;
+  user: User;
+};
