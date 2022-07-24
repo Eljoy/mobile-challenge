1cfa0de696f167da1ee0ba28a0eafb79c7b3dd82
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-07-24 16:29:50 +0400

    Reuse ExpenseInfo and EditExpenseComment

--- app/src/screens/ExpenseDetailsScreen.tsx
+++ app/src/screens/ExpenseDetailsScreen.tsx
@@ -1,10 +1,29 @@
 import { Layout } from '@design-system/layout/Layout';
+import { EditExpenseComment, ExpenseInfo } from '@features/expenses';
+import { useExpense } from '@features/expenses/useExpense';
+import { Expense } from '@models/Expense';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { RootStackParamList } from './RootStack';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Expenses'>;
+type Props = NativeStackScreenProps<RootStackParamList, 'ExpenseDetails'>;
 
-export const ExpenseDetailsScreen: React.FC<Props> = () => {
-  return <Layout />;
+export const ExpenseDetailsScreen: React.FC<Props> = ({ route }) => {
+  const { expense } = route.params;
+  const { updateExpenseComment } = useExpense();
+  const onSubmitComment = useCallback(
+    (comment: Expense['comment']) => {
+      updateExpenseComment(expense, comment);
+    },
+    [expense, updateExpenseComment]
+  );
+  return (
+    <Layout>
+      <ExpenseInfo expense={expense} />
+      <EditExpenseComment
+        comment={expense.comment}
+        updateComment={onSubmitComment}
+      />
+    </Layout>
+  );
 };
