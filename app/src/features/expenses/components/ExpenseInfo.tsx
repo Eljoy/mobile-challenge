376172faa6f839d003d0ddaaf9937b0fa17ad8d4
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-07 20:16:44 +0400

    Rework ExpenseInfo

--- app/src/features/expenses/components/ExpenseInfo.tsx
+++ app/src/features/expenses/components/ExpenseInfo.tsx
@@ -1,7 +1,9 @@
+import { Avatar, Spacer } from '@design-system/components';
 import { Layout } from '@design-system/layout/Layout';
+import { Font, FontColor, Text } from '@design-system/typography';
+
 import { Expense } from '@models/Expense';
 import React from 'react';
-import { Text } from 'react-native';
 
 export declare namespace ExpenseInfo {
   export type Props = {
@@ -12,9 +14,18 @@ export declare namespace ExpenseInfo {
 export const ExpenseInfo: React.FC<ExpenseInfo.Props> = React.memo(
   ({ expense }) => {
     return (
-      <Layout>
-        <Text>merchant {expense.merchant}</Text>
-        <Text>Paid by {expense.user.first + expense.user.last}</Text>
+      <Layout paddingScale={3} direction="row" align="start center">
+        <Avatar name={expense.merchant} />
+        <Spacer widthScale={3} />
+        <Layout>
+          <Text font={Font.Body1} color={FontColor.Primary}>
+            {expense.merchant}
+          </Text>
+          <Spacer heightScale={1} />
+          <Text font={Font.Caption} color={FontColor.Secondary}>
+            9:40
+          </Text>
+        </Layout>
       </Layout>
     );
   }
