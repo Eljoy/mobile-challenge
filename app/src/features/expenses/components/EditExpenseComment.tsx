375d358c7ea44901920afc9dd7fcb4a7df8394d4
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-07 20:16:28 +0400

    Rework EditExpenseComment

--- app/src/features/expenses/components/EditExpenseComment.tsx
+++ app/src/features/expenses/components/EditExpenseComment.tsx
@@ -1,7 +1,11 @@
 import { Layout } from '@design-system/layout/Layout';
+import { spaceScale } from '@design-system/lib';
+import { Font, FontColor, getFontStyle } from '@design-system/typography';
 import { Expense } from '@models/Expense';
 import React, { useCallback, useState } from 'react';
-import { TextInput } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
+import normalize from 'react-native-normalize';
+import Icon from 'react-native-vector-icons/Feather';
 
 export declare namespace EditExpenseComment {
   export type Props = {
@@ -21,12 +25,27 @@ export const EditExpenseComment: React.FC<EditExpenseComment.Props> = ({
   }, [commentState, updateComment]);
 
   return (
-    <Layout>
+    <Layout paddingScale={3} align="start center" direction="row">
+      <Icon
+        name="message-square"
+        size={normalize(16)}
+        color={FontColor.Secondary}
+      />
       <TextInput
+        style={[styles.textInput, getFontStyle(Font.Body2, FontColor.Primary)]}
         value={commentState}
         onChangeText={setCommentState}
         onSubmitEditing={onSubmit}
+        placeholder="Add Comment..."
+        selectionColor={FontColor.Primary}
       />
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  textInput: {
+    padding: spaceScale[1],
+    paddingHorizontal: spaceScale[2],
+  },
+});
