b6735b72e99efe5f2c8419475d9fcd356720f7b3
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-10 21:22:10 +0400

    Upgrade RootStack structure

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -4,13 +4,23 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { ExpenseDetailsScreen, ExpensesScreen, RootStack } from '@screens';
+import { StatusBar, StyleSheet } from 'react-native';
 import { store } from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <StatusBar barStyle="light-content" animated={true} />
       <NavigationContainer>
-        <RootStack.Navigator initialRouteName="Expenses">
+        <RootStack.Navigator
+          initialRouteName="Expenses"
+          screenOptions={{
+            headerStyle: styles.header,
+            contentStyle: styles.content,
+            headerBackTitleVisible: false,
+            headerTintColor: 'white',
+          }}
+        >
           <RootStack.Screen name="Expenses" component={ExpensesScreen} />
           <RootStack.Screen
             name="ExpenseDetails"
@@ -21,5 +31,12 @@ const App = () => {
     </Provider>
   );
 };
+
+const styles = StyleSheet.create({
+  header: { backgroundColor: '#EA3768' },
+  content: {
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
