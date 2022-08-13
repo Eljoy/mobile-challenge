01c2d1aef182c2157aec21b84641651543ebb24c
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-13 23:22:56 +0400

    Add test case for: uploadExpenseReceipt updateExpenseComment

--- app/src/features/expenses/store/expensesSlice.test.ts
+++ app/src/features/expenses/store/expensesSlice.test.ts
@@ -2,7 +2,10 @@ import {
   expenseSelectors,
   expensesSlice,
   fetchExpenses,
+  updateExpenseComment,
+  uploadExpenseReceipt,
 } from '@features/expenses';
+import { Expense } from '@models/Expense';
 import { generateExpense } from '@models/__test-utils__/generateExpense';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import * as api from '../api';
@@ -11,12 +14,12 @@ const rootReducer = combineReducers({
   expenses: expensesSlice.reducer,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
-});
-
 describe('expensesStore', () => {
   test('getExpenses', async () => {
+    const store = configureStore({
+      reducer: rootReducer,
+    });
+
     const expensesResult = {
       expenses: Array.from({ length: 5 }).map(() => generateExpense()),
       total: 10,
@@ -34,5 +37,70 @@ describe('expensesStore', () => {
 
     const resultExpenses = expenseSelectors.selectAll(store.getState());
     expect(resultExpenses).toEqual(expensesResult.expenses);
+  });
+
+  test('updateExpenseComment', async () => {
+    const store = configureStore({
+      reducer: rootReducer,
+    });
+
+    const expenses = Array.from({ length: 5 }).map(() => generateExpense());
+
+    const updatedExpense = { ...expenses[0], comment: 'testComment' };
+
+    jest
+      .spyOn(api, 'updateExpenseComment')
+      .mockReturnValue(Promise.resolve(updatedExpense));
+
+    store.dispatch({
+      type: fetchExpenses.fulfilled.type,
+      payload: { expenses },
+    });
+
+    store.dispatch({
+      type: updateExpenseComment.fulfilled.type,
+      payload: updatedExpense,
+    });
+
+    const resultExpense = expenseSelectors.selectById(
+      store.getState(),
+      updatedExpense.id
+    );
+
+    expect(resultExpense).toEqual(updatedExpense);
+  });
+
+  test('uploadExpenseReceipt', async () => {
+    const store = configureStore({
+      reducer: rootReducer,
+    });
+
+    const expenses = Array.from({ length: 5 }).map(() => generateExpense());
+
+    const updatedExpense: Expense = {
+      ...expenses[0],
+      receipts: [{ url: 'receiptUrl' }],
+    };
+
+    jest
+      .spyOn(api, 'uploadExpenseReceipt')
+      .mockReturnValue(Promise.resolve(updatedExpense));
+
+    store.dispatch({
+      type: fetchExpenses.fulfilled.type,
+      payload: { expenses },
+    });
+
+    store.dispatch({
+      type: uploadExpenseReceipt.fulfilled.type,
+      payload: updatedExpense,
+    });
+
+    const resultExpense = expenseSelectors.selectById(
+      store.getState(),
+      updatedExpense.id
+    );
+
+    expect(resultExpense).toEqual(updatedExpense);
   });
 });
