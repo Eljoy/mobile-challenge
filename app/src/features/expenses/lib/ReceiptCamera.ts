e834e0d55353e80c22ce290d4c0acd10d93cd65c
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-13 12:41:27 +0400

    Add ImageResizer

--- app/src/features/expenses/lib/ReceiptCamera.ts
+++ app/src/features/expenses/lib/ReceiptCamera.ts
@@ -1,15 +1,28 @@
 import { withPermissions } from '@common';
 import { Expense } from '@models/Expense';
-import { NativeModules } from 'react-native';
+import { Dimensions, NativeModules } from 'react-native';
+import ImageResizer from 'react-native-image-resizer';
 import { PERMISSIONS } from 'react-native-permissions';
-
 const { ReceiptCamera } = NativeModules;
 
 export const makeReceiptPhoto = withPermissions(
   async (expense: Expense): Promise<{ fileName?: string; uri?: string }> => {
-    return ReceiptCamera.makePhoto(
-      JSON.stringify({ formattedDate: '', ...expense })
-    );
+    const { width, height } = Dimensions.get('window');
+    const result: { fileName?: string; uri?: string } =
+      await ReceiptCamera.makePhoto(
+        JSON.stringify({ formattedDate: '', ...expense })
+      );
+    if (result?.uri) {
+      const { name, path } = await ImageResizer.createResizedImage(
+        result.uri,
+        2 * width,
+        2 * height,
+        'JPEG',
+        100
+      );
+      return { fileName: name, uri: path };
+    }
+    return {};
   },
   [PERMISSIONS.IOS.CAMERA],
   [PERMISSIONS.ANDROID.CAMERA]
