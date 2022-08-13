3cd89279cc2f41ff07992339ff793b65fb9c24c9
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-13 18:45:14 +0400

    Add react-native-permissions mock

--- app/jest.setup.js
+++ app/jest.setup.js
@@ -1,3 +1,8 @@
 import fetchMock from 'jest-fetch-mock';
+import mock from 'react-native-permissions/mock';
+
+jest.mock('react-native-permissions', () => {
+  return mock;
+});
 
 fetchMock.enableMocks();
