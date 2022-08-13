87e47cbbd9b05121d4e6f1828b6bb46366ff65a4
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-13 13:40:37 +0400

    Add updateExpenseComment test case

--- app/src/features/expenses/api.test.ts
+++ app/src/features/expenses/api.test.ts
@@ -5,6 +5,10 @@ import MockAdapter from 'axios-mock-adapter';
 const mock = new MockAdapter(axios);
 
 describe('expenses/api', () => {
+  beforeEach(() => {
+    mock.reset();
+  });
+
   test('getExpenses', async () => {
     const expenses = Array.from({ length: 10 }).map(() => generateExpense());
     mock.onGet(new RegExp('expenses', 'i')).reply(200, {
@@ -14,5 +18,18 @@ describe('expenses/api', () => {
     const { getExpenses } = require('@features/expenses/api');
     const result = await getExpenses();
     expect(result).toEqual({ expenses, total: expenses.length });
+  });
+
+  test('updateExpenseComment', async () => {
+    const expense = generateExpense();
+    mock.onPost(new RegExp('expenses', 'i')).reply((config) => {
+      const { url, data } = config;
+      expect(url?.endsWith(expense.id)).toBeTruthy();
+      expect(data).toEqual(JSON.stringify({ comment: expense.comment }));
+      return [200, expense];
+    });
+    const { updateExpenseComment } = require('@features/expenses/api');
+    const result = await updateExpenseComment(expense.id, expense.comment);
+    expect(result).toEqual(expense);
   });
 });
