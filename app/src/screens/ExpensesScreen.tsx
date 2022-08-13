d0faeedbccfae43f6c356045aa6e268506701118
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2022-08-13 18:43:53 +0400

    Add padding bottom

--- app/src/screens/ExpensesScreen.tsx
+++ app/src/screens/ExpensesScreen.tsx
@@ -5,6 +5,8 @@ import { useExpenseList } from '@features/expenses/hooks/useExpenseList';
 import { Expense } from '@models/Expense';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useCallback, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import normalize from 'react-native-normalize';
 import { RootStackParamList } from './RootStack';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Expenses'>;
@@ -24,6 +26,7 @@ export const ExpensesScreen: React.FC<Props> = ({ navigation }) => {
     <Layout flex={1}>
       <SearchBar onSearchKeywordChanged={setSearchKeyword} />
       <ExpenseList
+        contentContainerStyle={styles.container}
         expenses={expenses}
         onEndReached={triggerNext}
         onExpensePressed={onExpensePressed}
@@ -31,3 +34,9 @@ export const ExpensesScreen: React.FC<Props> = ({ navigation }) => {
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: normalize(36),
+  },
+});
